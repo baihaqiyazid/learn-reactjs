e54af8a67f07665711ff70b0864b97c0c0f00380
yazid <muhammad.baihaqi@solusi247.com>
2024-09-16 14:03:04 +0700

    learn rendering list

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,22 +1,48 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products =[
+    {
+        image: "../../public/images/shoes.jpg",
+        alt: "shoes",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi facere repellendus itaque veniam reiciendis maxime architecto? Magnam corporis cum voluptate eos iusto, placeat fugit quasi autem vitae laborum quia!",
+        title: "Flat Shoes",
+        price: "100.000"
+    },
+    {
+        image: "../../public/images/shoes.jpg",
+        alt: "shoes",
+        title: "Vans",
+        price: "200.000",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi facere repellendus itaque veniam reiciendis maxime architecto? Magnam corporis cum voluptate eos iusto, placeat fugit quasi autem vitae laborum quia!",
+    },
+    {
+        image: "../../public/images/shoes.jpg",
+        alt: "shoes",
+        title: "Adidas",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi facere repellendus itaque veniam reiciendis maxime architecto? Magnam corporis cum voluptate eos iusto, placeat fugit quasi autem vitae laborum quia!",
+        price: "300.000"
+    },
+    {
+        image: "../../public/images/shoes.jpg",
+        alt: "shoes",
+        title: "Nike",
+        description : "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum excepturi facere repellendus itaque veniam reiciendis maxime architecto? Magnam corporis cum voluptate eos iusto, placeat fugit quasi autem vitae laborum quia!",
+        price: "400.000"
+    },
+]
+
 const ProductPage = () => {
     return (
         <div className="flex justify-center">
-            <CardProduct>
-                <CardProduct.Header image="../../public/images/shoes.jpg" alt="shoes" />
-                <CardProduct.Body title="Flat Shoes">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis perspiciatis, ab inventore dolores dolor autem soluta eligendi. Dolore ducimus, eaque consectetur, a earum eum perspiciatis id amet exercitationem blanditiis aliquam!
+            {products.map((product, index) => (
+                <CardProduct>
+                <CardProduct.Header image={product.image} alt={product.alt} />
+                <CardProduct.Body title={product.title}>
+                    {product.description}
                 </CardProduct.Body>
-                <CardProduct.Footer price="100.000" />
+                <CardProduct.Footer price={product.price} />
             </CardProduct>
-            <CardProduct>
-                {/* <CardProduct.Header image="../../public/images/shoes.jpg" alt="shoes" /> */}
-                <CardProduct.Body title="Flat Shoes">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis perspiciatis, ab inventore dolores dolor autem soluta eligendi. Dolore ducimus, eaque consectetur, a earum eum perspiciatis id amet exercitationem blanditiis aliquam!
-                </CardProduct.Body>
-                <CardProduct.Footer price="50.000" />
-            </CardProduct>
+            ))}
         </div>
     );
 }
